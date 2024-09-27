a6d3b2af13a11e822f8289ba5f3cf2d41af48138
Jonas Bönhoff <28276430+boenhoff@users.noreply.github.com>
2024-09-27 11:16:36 +0200

    test: add github link

--- docs/conf.py
+++ docs/conf.py
@@ -26,6 +26,21 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 
-rst_prolog = """
-:github_url: https://github.com/bespin-studios/byteshard-docs
-"""
+html_context = {
+    "display_github": True,
+    "github_repo": "https://github.com/bespin-studios/byteshard-docs",
+    "github_version": "main",
+    "conf_py_path": "/docs/",
+}
+
+html_theme_options = {
+    "style_external_links": True,
+    "navigation_depth": 4,
+    "collapse_navigation": False,
+    "sticky_navigation": True,
+    "titles_only": False,
+    "display_github": True,
+    "github_repo": "https://github.com/bespin-studios/byteshard-docs"
+    "github_version": "main",
+    "conf_py_path": "/docs/",
+}
